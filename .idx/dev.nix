39bd15ad826aca66728fb6372bf691418d7b2370
tuomas.hatakka <tuomas.hatakka@gmail.com>
2025-07-06 21:18:30 +0000

    asd

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.bun
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "bun"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
